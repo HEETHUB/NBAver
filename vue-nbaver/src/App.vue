8fd0a2aa64636423c886eb7b520cff0cab96f877
Jaewoong Hur <jwhur7@naver.com>
2023-05-24 00:12:34 +0900

    20230524 12:12

--- vue-nbaver/src/App.vue
+++ vue-nbaver/src/App.vue
@@ -1,27 +1,66 @@
 <template>
+<div>
   <div id="app">
-    <header></header>
-    <nav>
-    <router-link to="/">
-      <img class="logo" src="./assets/NBAVER LOGO TRANSPARENT.png"/>
-    </router-link> 
-    <router-link to="/todays-game" class="r1" style="text-decoration:none">오늘의 경기</router-link>
-    <router-link to="/schedule" class="r2" style="text-decoration:none">경기 일정</router-link>
-    <router-link to="/highlight" class="r3" style="text-decoration:none">하이라이트 영상</router-link>
-    <router-link to="/news" class="r4" style="text-decoration:none">뉴스</router-link>
-    <router-link to="/ranking" class="r5" style="text-decoration:none">랭킹</router-link>
-    <router-link to="/player" class="r6" style="text-decoration:none">선수 정보</router-link>
-    <router-link to="/community" class="r7" style="text-decoration:none">커뮤니티</router-link>
-    <router-link to="/prediction-vote" class="r8" style="text-decoration:none">승부 예측</router-link>
-    <img src="./assets/homepic/menuicon.png" class="menu-icon" />
-    <router-link to="/login" class="r9" style="text-decoration:none">로그인</router-link>
-    <router-link to="/signup" class="r10" style="text-decoration:none">회원가입</router-link>
-    </nav>
-    <router-view/>
+    <div class="outer">
+      <div class="header">
+       <div class="category">
+          <div class="logo_area">
+            <router-link to="/">
+             <img class="logo" src="./assets/NBAVER LOGO TRANSPARENT.png"/>
+           </router-link>        
+           </div>
+            <div class="menu_area">  
+              <ul>
+                <li>
+                  <router-link to="/todays-game" class="r1">오늘의 경기</router-link>
+                </li>
+                <li>
+                  <router-link to="/schedule" class="r1">경기 일정</router-link>
+                </li>
+                <li>
+                   <router-link to="/highlight" class="r1">하이라이트 영상</router-link>
+                </li>
+                <li>
+                   <router-link to="/news" class="r1">뉴스</router-link>
+                </li>
+                <li>
+                   <router-link to="/ranking" class="r1">랭킹</router-link>
+                </li>
+                <li>
+                  <router-link to="/player" class="r1" >선수 정보</router-link>
+                </li>
+                <li>
+                   <router-link to="/community" class="r1">커뮤니티</router-link>
+                </li>
+                <li>
+                  <router-link to="/prediction-vote" class="r1">승부 예측</router-link>
+                </li>
+              </ul>
+           </div>
+          <div class="login_area">
+             <span class="r2">
+               <img src="./assets/homepic/menuicon.png"/>
+             </span>
+             <span class="r3">   
+               <router-link to="/login" class="r3" >로그인</router-link>  
+            </span>
+            <span class="r4">
+               <router-link to="/signup" class="r4">회원가입</router-link>
+           </span>       
+          </div>
+           <router-view></router-view>
+        </div>
+      </div>
+    </div>
+    <a href="#" class="btn_gotop">
+      <img src="./assets/homepic/topbutton.png" class="topbtn" />
+   </a>
   </div>
+</div>
 </template>
-   
-<style>
+
+<style scoped>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,34 +68,71 @@
   text-align: center;
   color: #2c3e50;
 }
-
-header {
-  background: linear-gradient(45deg,skyblue, lightGreen) ;
-  max-width:1920px;
-  width: 100%;
+.outer{
   height: 75px;
-  position: fixed;
-  top:0;
-  z-index : 9999;
+  position: relative;
 }
 
+.header {
+    background: linear-gradient(45deg,skyblue, lightgreen,turquoise) ;
+    border-bottom: 1px solid #d6d6d6;
+    box-sizing: content-box;
+    height: 75px;
+    width: 100%;
+    z-index: 30;
+}
+
+.logo_area{
+    float: left;
+    height: 100%;
+    text-align: left;
+    width: 180px;
+}
 .logo {
-  position: fixed;
-  top:15px;
-  left: 300px;
-  width: 135px;
-  height: 50px;
-  z-index : 9999;
+    display: inline-block;
+    text-align: left;
+    width: 135px;
+    top: 16px;
+    height: 45px;
+    position: relative;
+    vertical-align: middle;
 }
 
-.menu-icon {
-  position: fixed;
-  top: 26px;
-  left: 1390px;
-  z-index : 9999;
+.category{
+    height: 100%;
+    margin: 0 auto;
+    position: relative;
+    width: 1168px;
 }
 
-nav {
+.menu_area{
+    display: block;
+    float: left;
+    height: 100%;
+    position: relative;
+}
+.menu_area>ul{
+    display: inline-block;
+    height: 100%;
+    margin-top: 30px;
+    vertical-align: middle;
+    list-style: none;
+}
+.menu_area>ul>li{
+  display: inline-block;
+  height: 100%;
+}
+.menu_area>ul>li + li{
+  margin-left: 30px;
+}
+
+.login_area{
+    float: right;
+    height: 100%;
+}
+
+
+/* nav {
   padding: 30px;
 }
 
@@ -67,89 +143,50 @@ nav a {
 
 nav a.router-link-exact-active {
   /* color: #42b983; */
-  color: blue;
-}
+  /* color: blue;
+} */ 
 
 .r1 {
-  position: fixed;
-  top:30px;
-  left: 450px;
-  font-size: 15px;
-  color : white;
-  z-index : 9999;
+    margin: 0;
+    padding: 0;
+    font-size: 15px;
+    line-height: 18px;
+    text-decoration: none;
 }
-.r2 {
-  position: fixed;
-  top:30px;
-  left: 550px;
-  font-size: 15px;
-  color : white;
-  z-index : 9999;
+.r2{
+    display: inline-block;
+    height: 30px;
+    line-height: 30px;
+    margin-top: 22px;
+    margin-right: 10px;
+    position: relative;
+    text-align: center;
 }
-.r3 {
-  position: fixed;
-  top:30px;
-  left: 640px;
-  font-size: 15px;
-  color : white;
-  z-index : 9999;
+.r3{
+    position: relative;
+    margin-left:10px;
+    bottom: 6px;   
+    text-decoration: none;
 }
-.r4 {
-  position: fixed;
-  top:30px;
-  left: 770px;
-  font-size: 15px;
-  color : white;
-  z-index : 9999;
-}
-.r5 {
-  position: fixed;
-  top:30px;
-  left: 825px;
-  font-size: 15px;
-  color : white;
-  z-index : 9999;
-}
-.r6 {
-  position: fixed;
-  top:30px;
-  left: 880px;
-  font-size: 15px;
-  color : white;
-  z-index : 9999;
-}
-.r7 {
-  position: fixed;
-  top:30px;
-  left: 965px;
-  font-size: 15px;
-  color : white;
-  z-index : 9999;
-}
-.r8 {
-  position: fixed;
-  top:30px;
-  left: 1050px;
-  font-size: 15px;
-  color : white;
-  z-index : 9999;
-}
-.r9 {
-  position: fixed;
-  top:30px;
-  left: 1450px;
-  font-size: 15px;
-  color : white;
-  z-index : 9999;
-}
-.r10 {
-  position: fixed;
-  top:30px;
-  left: 1520px;
-  font-size: 15px;
-  color : white;
-  z-index : 9999;
+.r4{
+    position: relative; 
+    bottom:6px;
+    margin-left: 10px;
+    text-decoration: none;
 }
 
+.btn_gotop {
+display: block; 
+position: fixed; 
+bottom: 50px; 
+right: 40px; 
+z-index: 999; 
+outline: none; 
+cursor: pointer; 
 
+}
+.topbtn{
+  width: 50px;
+  height: 50px;
+}
 </style>
